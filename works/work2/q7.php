adaa95b9ca2c18a8e2a01033626f95aadba513a2
302A-14\cre <a>
2024-07-02 15:09:21 +0900

    q7

--- works/work2/q7.php
+++ works/work2/q7.php
@@ -9,3 +9,16 @@
 // 出力例 「johnの評価は良です」
 
 $scores = array("John" => 85, "Jane" => 92, "Jim" => 78);
+
+foreach ($scores as $key => $value){
+    if ($value >= 90){
+        $value = "優";
+    }elseif ($value >= 80 && $value < 90){
+        $value = "良";
+    }elseif ($value >= 70 && $value < 80){
+        $value = "可";
+    }elseif ($value < 70){
+        $value = "不可";
+    }
+    echo $key."の評価は".$value."です";
+}
