139acd50fb65d803b7350652e028fcbe9f0cf154
302A-13\cre <a>
2024-07-09 14:26:43 +0900

    q3

--- works/work3/q1.php
+++ works/work3/q1.php
@@ -2,8 +2,9 @@
 
 // 引数として受け取った数値を2倍にして返す関数を完成させなさい。
 
-double();
+double(50);
 
-function double()
+function double($double)
 {
+    echo $double * 2;
 }
--- works/work3/q3.php
+++ works/work3/q3.php
@@ -6,7 +6,12 @@ $values = array(1, 2, 3, 4, 5);
 $result = average();
 echo "平均値: " . $result . "\n";
 
+for ($i = 1; $i <= count ($values); $i++) {
+    echo "$values[$i]";
+}
 
 function average()
 {
+    
+    return $result;
 }
