275c3cad6233498ad14fdcaae3a20c6a536838ff
302A-13\cre <a>
2024-07-16 13:36:46 +0900

    q3

--- works/work3/q3.php
+++ works/work3/q3.php
@@ -2,14 +2,17 @@
 
 // 配列を引数として受け取り、その配列内の数値の平均値を計算して返す関数を完成させなさい
 
-$value = array(1, 2, 3, 4, 5);
+$value = array(1, 2, 3, 4, 5,6,7);
 $result = average($value);
 echo "平均値: " . $result . "\n";
 
 function average($values)
 {   
-    for ($i = 1; $i < count($values); $i++)
+    $values_size = count($values);
+    $total = 0;
+    for ($i = 0; $i < $values_size; $i++)
     {
-        echo "$values";
+        $values[$i] = $total += $values[$i];
     }
+    return $total / $values_size;
 }
