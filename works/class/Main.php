9c284db139b00320e8c961fbd300eea4dbde59ec
302A-14\cre <a>
2024-09-03 13:36:15 +0900

    kansei

--- works/class/Main.php
+++ works/class/Main.php
@@ -7,32 +7,12 @@ $mainWeapon = new Gun("AK47", 30);
 $subWeapon = new Gun("Mosin-Nagant", 5);
 
 // 現在の状態を表示
-$mainWeapon->echoStatus();
-$subWeapon->echoStatus();
 
-$mainWeapon->reload();
-$subWeapon->reload();
 
 $mainWeapon->echoStatus();
 $subWeapon->echoStatus();
 
-$mainWeapon->fire();
-$subWeapon->fire();
 
-$mainWeapon->echoStatus();
-$subWeapon->echoStatus();
-
-$mainWeapon->setExtendedMagazine(20);
-$subWeapon->setExtendedMagazine(5);
-
-$mainWeapon->echoStatus();
-$subWeapon->echoStatus();
-
-$mainWeapon->fire();
-$subWeapon->fire();
-
-$mainWeapon->echoStatus();
-$subWeapon->echoStatus();
 
 $mainWeapon->unsetExtendedMagazine();
 $subWeapon->unsetExtendedMagazine();
